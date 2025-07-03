05ed06f050da8df3efb7d014bac0f35f69ff3939
arthur-felipe-as <arthur.f.andrade.s@gmail.com>
2025-07-03 09:07:41 -0300

    cores e paleta

---
+++ cabecalho.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+struct Cor
+{
+    unsigned int r, g, b;
+};
+
+
+class Paleta{
+    int quant;
+    std::vector <Cor> cores;
+    std::vector <int> valores; 
+    
+};
+
