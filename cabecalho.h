249a23a086b3bda1e24eb4b11014a0c1992cb964
arthur-felipe-as <arthur.f.andrade.s@gmail.com>
2025-07-04 10:17:24 -0300

    função de leitura

--- cabecalho.h
+++ cabecalho.h
@@ -1,5 +1,4 @@
 
-
 struct Cor
 {
     unsigned int r, g, b;
@@ -8,9 +7,12 @@ struct Cor
 class Paleta{
     int quantidade;
     Cor cores[100];
-    int altura[100]; 
+    int altura[100];
+
     public:
+    int getquantidade();
+    Cor getcor();
     Paleta(int q, Cor c[], int a[]);
-    Paleta Ler_arquivo();
+    void Ler_arquivo(string arquivo);
     Cor Consulta_cor(int a);
 };
--- classes.cpp
+++ classes.cpp
@@ -1,31 +1,47 @@
 #include "cabecalho.h"
 #include <fstream>
+#include <string>
+#include <iostream>
+
 using namespace std;
+
+int Paleta::getquantidade(){
+    return quantidade;
+}
 
 Paleta::Paleta(int q, Cor c[], int a[]){
     quantidade = q;
+
     for(int i = 0; i < q; i++){
-        altura[i] = a[i];
         cores[i] = c[i];
     }
-}
-
-Paleta Ler_arquivo(fstream arquivo){   
-    Paleta x; //Problema de inicialização
-    ios::in;
-    x << arquivo; //Problema de definição
-    return x;
-
-}
-
-Cor Consulta_cor(Paleta paleta, int valor){ 
-    for(int i = 0; i < paleta.quantidade //Valor privado
-        if
+    for(int i = 0; i < q; i++){
+     altura[i] = a[i];
     }
 
 }
 
+void Paleta::Ler_arquivo(string arquivo){
+    fstream canal(arquivo);
+    canal >> quantidade;
+
+    for(int i = 0; i < quantidade; i++){
+        canal >> altura[i];
+        Cor temporaria;
+        int unsigned r, g, b;
+        canal >> r >> g >> b;
+        temporaria = {r, g, b};
+        cores[i] = temporaria;
+    }
+
+}
+
+
 int main(){
-    
+    Paleta x;
+    string teste = "teste.txt";
+    x.Ler_arquivo(teste);
+    cout << x.getquantidade() << endl;
+
     return 0;
 }
